c4d6a6be1baf15246d2eff792a22fb5c18cc7fe4
sbrissen <sbrissen@hotmail.com>
2014-02-26 15:25:39 -0500

    t0ltevzw: update fingerprint and description
    Change-Id: I5f372d6950132c2be4d0059153c87dde7bd0d89b

--- omni_i605.mk
+++ omni_i605.mk
@@ -31,4 +31,4 @@ PRODUCT_MODEL := SCH-I605
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.1.2/JZO54K/I605VRAMC3:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.1.2 JZO54K I605VRAMC3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.3/JSS15J/I605VRUEMJ9:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.3 JSS15J I605VRUEMJ9 release-keys"
